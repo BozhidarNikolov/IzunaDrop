68b96638846c39f2884151ae46e9ee87270d0068
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-11-16 23:11:51 +0200

    Created Enemy data model
    Created Enemy data model.

---
+++ Data/Models/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IzunaDrop.Data.Models
+{
+    public class Enemy
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+
+        public string Name { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
