eea0007815f35a9593e522ce90f00832ce560505
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-11-17 00:01:01 +0200

    Created BaseModelConstants
    Created BaseModelConstants. Abstract class for all constants to inherit from

---
+++ Constants/BaseModelConstant.cs
@@ -0,0 +1,10 @@
+﻿namespace IzunaDrop.Constants
+{
+    public abstract class BaseModelConstant
+    {
+        public const int NameMaxLength = 100;
+        public const int NameMinLength = 1;
+        public const int DescriptionMaxLength = 500;
+        public const int DescriptionMinLength = 10;
+    }
+}
