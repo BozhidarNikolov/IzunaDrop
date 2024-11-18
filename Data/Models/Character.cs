922b2d51eeb0728377d10ea8e404f846479e69b5
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-11-19 00:38:15 +0200

    Create Character Model
    Create Character model

--- Data/Models/Character.cs
+++ Data/Models/Character.cs
@@ -1,6 +1,30 @@
-﻿namespace IzunaDrop.Data.Models
+﻿using IzunaDrop.Constants;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace IzunaDrop.Data.Models
 {
     public class Character
     {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        [MinLength(CharacterConstants.NameMinLength)]
+        [MaxLength(CharacterConstants.NameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [MinLength(CharacterConstants.DescriptionMinLength)]
+        [MaxLength(CharacterConstants.DescriptionMaxLength)]
+        public string Description { get; set; } = string.Empty;
+
+
+        public string? ImagePath { get; set; }
+
+        [ForeignKey(nameof(GameId))]
+        public int GameId { get; set; }
+        public Game Game { get; set; } = null!;
     }
 }
