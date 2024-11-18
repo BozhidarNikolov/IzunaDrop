123aefe6200f750db092af4a24154a85ba7dacbc
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-11-19 00:30:08 +0200

    Updated Item model
    Populated Item model with props

--- Data/Models/Item.cs
+++ Data/Models/Item.cs
@@ -1,7 +1,30 @@
-﻿namespace IzunaDrop.Data.Models
+﻿using IzunaDrop.Constants;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IzunaDrop.Data.Models
 {
     public class Item
     {
+        [Required]
+        [Key]
+        public int Id { get; set; }
 
+
+        [Required]
+        [MinLength(ItemConstants.NameMinLength)]
+        [MaxLength(ItemConstants.NameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        [MinLength(ItemConstants.DescriptionMinLength)]
+        [MaxLength(ItemConstants.DescriptionMaxLength)]
+        public string Description { get; set; } = string.Empty;
+
+
+        public string? ImagePath { get; set; }
+
+        [ForeignKey(nameof(GameId))]
+        public int GameId { get; set; }
+        public Game Game { get; set; } = null!;
     }
 }
