382baaf6a5d7fa564723c0e810dd2555a5932793
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-11-23 00:51:42 +0200

    Update HomeController.cs
    Add _context and constructor  to homecontroller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using IzunaDrop.Data;
 using IzunaDrop.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -7,14 +8,18 @@ namespace IzunaDrop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IzunaDropDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IzunaDropDbContext context,ILogger<HomeController> logger)
         {
+            _context = context;
             _logger = logger;
         }
+       
 
         public IActionResult Index()
         {
+            
             return View();
         }
 
