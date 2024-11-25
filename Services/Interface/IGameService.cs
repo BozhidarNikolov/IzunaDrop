442bc67503fee92c6b527a1ee704fd2400cd7f48
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-11-25 19:38:58 +0200

    Create IGameService.cs
    Create interface for GameService

---
+++ Services/Interface/IGameService.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using IzunaDrop.Data.Models;
+namespace IzunaDrop.Services.Interface
+{
+    public interface IGameService
+    {
+        Task<IEnumerable<Game>> GetAllGamesAsync();
+    }
+}
