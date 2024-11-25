fce1dd73c338dcb934d329fa0bf216a42e017038
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-11-25 19:43:13 +0200

    Create GameService
    Create GameService and create GetAllGamesAsync method

---
+++ Services/GameService.cs
@@ -0,0 +1,22 @@
+﻿using IzunaDrop.Data;
+using IzunaDrop.Data.Models;
+using IzunaDrop.Services.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace IzunaDrop.Services
+{
+    public class GameService:IGameService
+    {
+        private readonly IzunaDropDbContext _context;
+
+        public GameService(IzunaDropDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IEnumerable<Game>> GetAllGamesAsync()
+        {
+            return await _context.Games.ToListAsync();
+        }
+    }
+}
