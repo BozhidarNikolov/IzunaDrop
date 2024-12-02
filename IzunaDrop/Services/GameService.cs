86ea67d12d15b1574425c1088ff3f5c7fb2d7de8
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-02 14:43:37 +0200

    Update GameService.cs
    Implement GetGameByIdAsync

--- IzunaDrop/Services/GameService.cs
+++ IzunaDrop/Services/GameService.cs
@@ -18,5 +18,12 @@ namespace IzunaDrop.Services
         {
             return await _context.Games.ToListAsync();
         }
+
+        public async Task<Game> GetGameByIdAsync(int id)
+        {
+            return await _context.Games
+              .FirstOrDefaultAsync(g => g.Id == id);
+            
+        }
     }
 }
