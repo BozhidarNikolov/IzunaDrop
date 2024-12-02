2426eb8ac6fc6ee44cec4da9a182c6b239e10ccb
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-02 15:14:26 +0200

    Create GameController.cs
    Create GameController, and the Details action

---
+++ IzunaDrop/Controllers/GameController.cs
@@ -0,0 +1,27 @@
+﻿using IzunaDrop.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IzunaDrop.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly IGameService _gameService;
+        private readonly ILogger<GameController> _logger;
+
+        public GameController(IGameService gameService, ILogger<GameController> logger)
+        {
+            _gameService = gameService;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var game = await _gameService.GetGameByIdAsync(id);
+            if (game==null)
+            {
+                return View("NotFound"); 
+            }
+            return View(game);
+        }
+    }
+}
