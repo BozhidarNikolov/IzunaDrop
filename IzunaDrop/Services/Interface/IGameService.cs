06f161b68e8c2964906000b9812a42c009bc93fe
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-02 14:39:11 +0200

    Update IGameService.cs
    Add GetGameByIdAsync to IGameService interface

--- IzunaDrop/Services/Interface/IGameService.cs
+++ IzunaDrop/Services/Interface/IGameService.cs
@@ -7,5 +7,6 @@ namespace IzunaDrop.Services.Interface
     public interface IGameService
     {
         Task<IEnumerable<Game>> GetAllGamesAsync();
+        Task<Game> GetGameByIdAsync(int id);
     }
 }
