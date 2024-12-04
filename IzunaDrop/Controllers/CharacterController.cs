e7d039205305f1018a8a975f2323de57fd7b3748
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-04 21:12:19 +0200

    Create CharacterController.cs
    Create CharacterController

---
+++ IzunaDrop/Controllers/CharacterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IzunaDrop.Controllers
+{
+    public class CharacterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
