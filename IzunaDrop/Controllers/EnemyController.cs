61345fab4f593b1f236f2fc83305360e89f35e7b
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-04 21:38:55 +0200

    Create EnemyController.cs
    Create EnemyController

---
+++ IzunaDrop/Controllers/EnemyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IzunaDrop.Controllers
+{
+    public class EnemyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
