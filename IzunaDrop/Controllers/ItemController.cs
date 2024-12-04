dd9c5bbe858866caf01f07a60fc4d79a6436c2ba
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-04 21:39:34 +0200

    Create ItemController.cs
    Create ItemController

---
+++ IzunaDrop/Controllers/ItemController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IzunaDrop.Controllers
+{
+    public class ItemController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
