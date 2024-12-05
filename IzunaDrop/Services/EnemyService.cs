3499573c5d47c33f05d7cc1be66ce7fab4a680d0
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-05 22:01:40 +0200

    Create EnemyService.cs
    Create EnemyService. Add Dbcontext to it

---
+++ IzunaDrop/Services/EnemyService.cs
@@ -0,0 +1,14 @@
+﻿using IzunaDrop.Data;
+using IzunaDrop.Services.Interface;
+
+namespace IzunaDrop.Services
+{
+    public class EnemyService:IEnemyService
+    {
+        private readonly IzunaDropDbContext _context;
+        public EnemyService(IzunaDropDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
