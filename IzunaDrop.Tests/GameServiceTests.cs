c6b348aa359334d19f534592fadc2183f58c8b94
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-06 23:14:53 +0200

    Update GameServiceTests.cs
    Create test for GetAllEnemiesAsync

--- IzunaDrop.Tests/GameServiceTests.cs
+++ IzunaDrop.Tests/GameServiceTests.cs
@@ -2,6 +2,7 @@ using IzunaDrop.Data;
 using IzunaDrop.Data.Models;
 using IzunaDrop.Services;
 using IzunaDrop.Services.Interface;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 
 namespace IzunaDrop.Tests
@@ -9,6 +10,7 @@ namespace IzunaDrop.Tests
     public class GameServiceTests
     {
         private IGameService _gameService;
+        private IEnemyService _enemyService;
         private IzunaDropDbContext _context;
 
         private void InitializeDatabase()
@@ -23,9 +25,17 @@ namespace IzunaDrop.Tests
                 new Game {Id=1, Name="Game 1", Description="Test game 1", ReleaseDate=new DateTime(2012,12,12)},
                 new Game {Id=2, Name="Game 2", Description="Test game 2",ReleaseDate=new DateTime(2010,10,10)}
             });
+            _context.Enemies.AddRange(new List<Enemy>
+            {
+                new Enemy {Id=1,Name="Enemy 1",Description="Enemy test 1",GameId=1 },
+                new Enemy {Id=2, Name="Enemy 2",Description="Enemy test 2",GameId=2},
+                new Enemy {Id=3, Name="Enemy 3",Description="Enemy test 3",GameId=2}
+
+            });
             _context.SaveChanges();
 
             _gameService = new GameService(_context);
+            _enemyService = new EnemyService(_context);
 
         }
 
@@ -65,6 +75,15 @@ namespace IzunaDrop.Tests
 
             
             Assert.Null(result); 
+        }
+        [Fact]
+        public async Task GetAllEnemiesAsync_ShouldReturnAllEnemies()
+        {
+            InitializeDatabase();
+            int gameId = 2;
+            var result = await _enemyService.GetAllEnemiesAsync(gameId);
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count());
         }
     }
 }
