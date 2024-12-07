85f5ddeed8a1098d6505a6eb2e75c7db09794e52
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-07 21:42:43 +0200

    Create CharacterServiceTests.cs
    Create and set up CharacterServiceTests

---
+++ IzunaDrop.Tests/CharacterServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using IzunaDrop.Data;
+using IzunaDrop.Data.Models;
+using IzunaDrop.Services;
+using IzunaDrop.Services.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IzunaDrop.Tests
+{
+    public class CharacterServiceTests
+    {
+        private ICharacterService _characterService;
+        private IzunaDropDbContext _context;
+
+        private void InitializeDatabase()
+        {
+            var options = new DbContextOptionsBuilder<IzunaDropDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+            _context = new IzunaDropDbContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Games.AddRange(new List<Game>
+            {
+                new Game {Id=1, Name="Game 1", Description="Test game 1", ReleaseDate=new DateTime(2012,12,12)},
+                new Game {Id=2, Name="Game 2", Description="Test game 2",ReleaseDate=new DateTime(2010,10,10)}
+            });
+            _context.Characters.AddRange(new List<Character>
+            {
+                new Character {Id=1,Name="Character 1",Description="Character test 1",GameId=1},
+                new Character {Id=2, Name="Character 2",Description="Character test 2",GameId=2},
+                new Character {Id=3, Name="Character 3",Description="Character test 3",GameId=2}
+
+            });
+            _context.SaveChanges();
+
+            _characterService = new CharacterService(_context);
+
+        }
+        
+    }
+}
