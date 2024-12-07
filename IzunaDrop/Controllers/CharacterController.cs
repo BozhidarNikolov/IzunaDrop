bfaeda34038cabb8af4a9e15d85de0bf47a001a0
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-07 21:04:59 +0200

    Update CharacterController.cs
    Add _logger and _context to CharacterController

--- IzunaDrop/Controllers/CharacterController.cs
+++ IzunaDrop/Controllers/CharacterController.cs
@@ -1,9 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IzunaDrop.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IzunaDrop.Controllers
 {
     public class CharacterController : Controller
     {
+        private readonly ILogger<CharacterController> _logger;
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService, ILogger<CharacterController> logger)
+        {
+            _characterService = characterService;
+            _logger = logger;
+        }
         public IActionResult Index()
         {
             return View();
