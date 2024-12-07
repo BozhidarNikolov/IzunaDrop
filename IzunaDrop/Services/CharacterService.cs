6e7c8b512a1b2b54cf01550e670362a5708148aa
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-07 20:12:58 +0200

    Create CharacterService.cs
    Create CharacterService and initialize DbContext in it

---
+++ IzunaDrop/Services/CharacterService.cs
@@ -0,0 +1,21 @@
+﻿using IzunaDrop.Data;
+using IzunaDrop.Data.Models;
+using IzunaDrop.Services.Interface;
+
+namespace IzunaDrop.Services
+{
+    public class CharacterService : ICharacterService
+    {
+        private readonly IzunaDropDbContext _context;
+
+        public CharacterService(IzunaDropDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<IEnumerable<Character>> GetAllCharactersAsync(int gameId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
