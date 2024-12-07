9c7c8a01541b9006f4fef6bc5bf0bcd2d2aac837
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-07 21:08:14 +0200

    Update CharacterController.cs
    Make Character Index() Async

--- IzunaDrop/Controllers/CharacterController.cs
+++ IzunaDrop/Controllers/CharacterController.cs
@@ -13,7 +13,7 @@ namespace IzunaDrop.Controllers
             _characterService = characterService;
             _logger = logger;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             return View();
         }
