b497e9863580a8e3e16f827d7391453b16ff215b
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-09 22:18:36 +0200

    Create DummyEmailSender.cs
    Add a dummy IEmailSender

---
+++ IzunaDrop/Services/DummyEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace IzunaDrop.Services
+{
+    public class DummyEmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            // Do nothing, just return completed task.
+            return Task.CompletedTask;
+        }
+    }
+}
