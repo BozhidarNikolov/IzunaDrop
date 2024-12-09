ef2803922a5664542733fd5e9179768f155c80c5
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-09 20:57:18 +0200

    Create ItemService.cs
    Create ItemService and implement GetAllItemsAsync

---
+++ IzunaDrop/Services/ItemService.cs
@@ -0,0 +1,28 @@
+﻿using IzunaDrop.Data;
+using IzunaDrop.Data.Models;
+using IzunaDrop.Services.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace IzunaDrop.Services
+{
+    public class ItemService : IItemService
+    {
+        private readonly IzunaDropDbContext _context;
+
+        public ItemService(IzunaDropDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Item>> GetAllItemsAsync(int gameId)
+        {
+            return await _context.Items
+                .Where(i => i.GameId == gameId)
+                .ToListAsync();
+        }
+
+        public Task<Item> GetItemByIdAsync(int gameId, int itemId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
