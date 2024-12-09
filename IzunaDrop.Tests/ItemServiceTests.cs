0ed4186a336fceeec4c4426e641330dfd5e8a9dd
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-09 21:10:49 +0200

    Create ItemServiceTests.cs
    Create and Set up ItemServiceTests

---
+++ IzunaDrop.Tests/ItemServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using IzunaDrop.Data.Models;
+using IzunaDrop.Data;
+using IzunaDrop.Services.Interface;
+using IzunaDrop.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IzunaDrop.Tests
+{
+    public class ItemServiceTests
+    {
+        private IItemService _itemService;
+        private IzunaDropDbContext _context;
+
+        private void InitializeDatabase()
+        {
+            var options = new DbContextOptionsBuilder<IzunaDropDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+            _context = new IzunaDropDbContext(options);
+            _context.Database.EnsureDeleted();
+            _context.Games.AddRange(new List<Game>
+            {
+                new Game {Id=7, Name="Game 1", Description="Test game 1", ReleaseDate=new DateTime(2012,12,12)},
+                new Game {Id=8, Name="Game 2", Description="Test game 2",ReleaseDate=new DateTime(2010,10,10)}
+            });
+            _context.SaveChanges();
+
+            _itemService = new ItemService(_context);
+        }
+    }
+}
