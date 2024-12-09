eb162ca9203c4096351dfe48ef586ce180f8d8f3
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-09 20:49:43 +0200

    Create IItemService.cs
    Create IItemService and populate it

---
+++ IzunaDrop/Services/Interface/IItemService.cs
@@ -0,0 +1,10 @@
+﻿using IzunaDrop.Data.Models;
+
+namespace IzunaDrop.Services.Interface
+{
+    public interface IItemService
+    {
+        Task<IEnumerable<Item>> GetAllItemsAsync(int gameId);
+        Task<Item> GetItemByIdAsync(int gameId, int itemId);
+    }
+}
