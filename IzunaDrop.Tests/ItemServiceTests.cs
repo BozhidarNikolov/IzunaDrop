8c1a896ffdbca7af70f2ec0ef9b241e16f912127
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-09 21:14:23 +0200

    Update ItemServiceTests.cs
    Add Items to test database in ItemServiceTests

--- IzunaDrop.Tests/ItemServiceTests.cs
+++ IzunaDrop.Tests/ItemServiceTests.cs
@@ -28,6 +28,11 @@ namespace IzunaDrop.Tests
                 new Game {Id=7, Name="Game 1", Description="Test game 1", ReleaseDate=new DateTime(2012,12,12)},
                 new Game {Id=8, Name="Game 2", Description="Test game 2",ReleaseDate=new DateTime(2010,10,10)}
             });
+            _context.Items.AddRange(new List<Item>
+            {
+                new Item{Id=1,Name="Item 1",Description="Test item 1"},
+                new Item{Id=2,Name="Item 2",Description="Test item 2"}
+            });
             _context.SaveChanges();
 
             _itemService = new ItemService(_context);
