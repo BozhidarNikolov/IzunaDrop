75c9356def5b704e7c74a74f9b7a64f9cb344518
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-09 21:05:42 +0200

    Update ItemController.cs
    Add ItemService to itemController

--- IzunaDrop/Controllers/ItemController.cs
+++ IzunaDrop/Controllers/ItemController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using IzunaDrop.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace IzunaDrop.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly IItemService _itemService;
+        private readonly ILogger<ItemController> _logger;
+
+        public ItemController(IItemService itemService, ILogger<ItemController> logger)
+        {
+            _itemService = itemService;
+            _logger = logger;
+        }
         public IActionResult Index()
         {
+            
             return View();
         }
     }
