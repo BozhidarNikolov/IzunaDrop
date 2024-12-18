dd45832079e3fd50497c5901ed8f75bb0a1f0056
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-18 22:43:39 +0200

    Update EnemyService.cs
    Implement full CRUD in EnemyService

--- IzunaDrop/Services/EnemyService.cs
+++ IzunaDrop/Services/EnemyService.cs
@@ -13,6 +13,25 @@ namespace IzunaDrop.Services
             _context = context;
         }
 
+        public async Task<Enemy> CreateEnemyAsync(Enemy enemy)
+        {
+            _context.Enemies.Add(enemy);
+            await _context.SaveChangesAsync();
+            return enemy;
+        }
+
+        public async Task<bool> DeleteEnemyAsync(int enemyId)
+        {
+            var enemy = await _context.Enemies.FindAsync(enemyId);
+            if (enemy == null)
+            {
+                return false;
+            }
+            enemy.IsDeleted = true;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
         public async Task<IEnumerable<Enemy>> GetAllEnemiesAsync(int gameId)
         {
             return await _context.Enemies
@@ -26,6 +45,23 @@ namespace IzunaDrop.Services
                 .Where(e => e.GameId == gameId && e.Id == enemyId)
                 .FirstOrDefaultAsync();
                
+        }
+
+        public async Task<bool> UpdateEnemyAsync(Enemy updatedEnemy)
+        {
+            var existingEnemy = await _context.Enemies.FindAsync(updatedEnemy.Id);
+            if (existingEnemy == null)
+            {
+                return false;
+            }
+
+            existingEnemy.Name = updatedEnemy.Name;
+            existingEnemy.Description = updatedEnemy.Description;
+            existingEnemy.GameId = updatedEnemy.GameId;
+
+            _context.Enemies.Update(existingEnemy);
+            await _context.SaveChangesAsync();
+            return true;
         }
     }
 }
