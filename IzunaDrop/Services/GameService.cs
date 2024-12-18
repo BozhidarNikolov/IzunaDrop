c27622b8af7ae2b5645aaaf147b9b769c13a8cc2
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-18 23:08:56 +0200

    Update GameService.cs
    Implement full CRUD to GameService

--- IzunaDrop/Services/GameService.cs
+++ IzunaDrop/Services/GameService.cs
@@ -13,7 +13,26 @@ namespace IzunaDrop.Services
         {
             _context = context;
         }
-        
+
+        public async Task<Game> CreateGameAsync(Game game)
+        {
+            _context.Games.Add(game);
+            await _context.SaveChangesAsync();
+            return game;
+        }
+
+        public async Task<bool> DeleteGameAsync(int gameId)
+        {
+            var game = await _context.Games.FindAsync(gameId);
+            if (game == null)
+            {
+                return false;
+            }
+            game.IsDeleted = true;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
         public async Task<IEnumerable<Game>> GetAllGamesAsync()
         {
             return await _context.Games.ToListAsync();
@@ -24,6 +43,22 @@ namespace IzunaDrop.Services
             return await _context.Games
               .FirstOrDefaultAsync(g => g.Id == id);
             
+        }
+
+        public async Task<bool> UpdateGameAsync(Game updatedGame)
+        {
+            var existingGame = await _context.Games.FindAsync(updatedGame.Id);
+            if (existingGame == null)
+            {
+                return false;
+            }
+            existingGame.Name = updatedGame.Name;
+            existingGame.Description = updatedGame.Description;
+            existingGame.ImagePath = updatedGame.ImagePath;
+
+            _context.Games.Update(existingGame);
+            await _context.SaveChangesAsync();
+            return true;
         }
     }
 }
