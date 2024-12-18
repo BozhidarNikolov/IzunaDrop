791b429c429090b0e4b860bdd78e42f62d66777d
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-18 23:23:43 +0200

    Update ItemService.cs
    Implement CRUD in ItemService

--- IzunaDrop/Services/ItemService.cs
+++ IzunaDrop/Services/ItemService.cs
@@ -13,6 +13,26 @@ namespace IzunaDrop.Services
         {
             _context = context;
         }
+
+        public async Task<Item> CreateItemAsync(Item item)
+        {
+             _context.Items.Add(item);
+            await _context.SaveChangesAsync();
+            return item;
+        }
+
+        public async Task<bool> DeleteItemAsync(int itemId)
+        {
+            var item = await _context.Items.FindAsync(itemId);
+            if (item == null)
+            {
+                return false;
+            }
+            item.IsDeleted = true;
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
         public async Task<IEnumerable<Item>> GetAllItemsAsync(int gameId)
         {
             return await _context.Items
@@ -25,6 +45,22 @@ namespace IzunaDrop.Services
             return await _context.Items
                 .Where(i => i.GameId == gameId && i.Id == itemId)
                 .FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> UpdateItemAsync(Item updatedItem)
+        {
+            var existingItem = await _context.Items.FindAsync(updatedItem.Id);
+            if (existingItem == null)
+            {
+                return false;
+            }
+            existingItem.Name = updatedItem.Name;
+            existingItem.Description = updatedItem.Description;
+            existingItem.GameId = updatedItem.GameId;
+            existingItem.ImagePath = updatedItem.ImagePath;
+            _context.Items.Update(existingItem);
+            await _context.SaveChangesAsync();
+            return true;
         }
     }
 }
