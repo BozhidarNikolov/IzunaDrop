4512eccb3f901db715072886517c185091a4c025
Bozhidar Nikolaev Nikolov <bozhidarnikolov281102@gmail.com>
2024-12-19 23:13:59 +0200

    Add GameCreateViewModel to ViewModels
    Add GameCreateViewModel to ViewModels

---
+++ IzunaDrop.ViewModels/GameCreateViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace IzunaDrop.ViewModels
+{
+    public class GameCreateViewModel
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 3)] // Adjust to your constants if needed
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(250, MinimumLength = 10)] // Adjust to your constants if needed
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public string Genre { get; set; }
+
+        [Required]
+        public DateTime? ReleaseDate { get; set; }
+
+        [Required]
+        public string Developer { get; set; } = string.Empty;
+
+        [Required]
+        public string Publisher { get; set; } = string.Empty;
+
+        public string? ImagePath { get; set; }
+    }
+}
